b024ab3a0ef34739d78e4d0cfddb4656a3c26a46
karthipannerselvam <118588657+karthipannerselvam@users.noreply.github.com>
2024-02-12 11:44:34 +0530

    Installed new app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodolistComponent } from "./todolist/todolist.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, TodolistComponent]
 })
 export class AppComponent {
   title = '  hi firstapp';
